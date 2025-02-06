5cc6736515cfaec4869017232a04c18e7fd35b4d
Devon Morrison <devmorri421@gmail.com>
2025-02-06 13:20:33 -0600

    added gitignore and some calendar code.

--- text_reminder.py
+++ text_reminder.py
@@ -1,10 +1,15 @@
 import os
 # import getpass
 # USER_NAME = getpass.getuser()
-
+import datetime
 from write_startup import add_to_startup
 
 #? Adds script to windows startup
 #add_to_startup()
+
+#? Retrieves system date for storage and comparison
+now = datetime.datetime.now()
+current_date = now.date()
+print(current_date)
 
 #os.system('start cmd')
