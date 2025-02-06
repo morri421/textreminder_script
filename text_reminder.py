0f2a365842856df135937b3e7bd4c87f74f24d76
Devon Morrison <devmorri421@gmail.com>
2025-02-06 14:11:48 -0600

    added import for twilio library. verified it.

--- text_reminder.py
+++ text_reminder.py
@@ -2,8 +2,13 @@ import os
 # import getpass
 # USER_NAME = getpass.getuser()
 import datetime
+
+# import local env variables
 from dotenv import load_dotenv, dotenv_values
+
 from write_startup import add_to_startup
+# import twilio library for sms functionality
+from twilio import Client
 
 #? Adds script to windows startup
 #add_to_startup()
